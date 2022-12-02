82c588dfebf5aac35242ff3b0bce85410bc2f8b8
Hero <76834176+jish0101@users.noreply.github.com>
2022-12-02 12:04:43 +0530

    useState-hook!

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  // first we declare initial state of the component by callling useState function which returns an array with two objects so we usuallly destructure them-
+  
+  // useState() gets a callback function to set initial state and to avoid calling it again on re-rendering everytime.. in this example it doesnt matter much because affect is minor but it can affect if it is complex.. useState(()=>{code to return intial state here..}) and ofcouse we can call another function in this callback function which will also run once;
+
+  // in this example first state function is used to save the current state of the component and second is used to update and OVERWRITE old state; we cant change current state directly to avoid state clashes when calling 2nd function more than once because it override itself so second function takes a callback function which is used to update state by passing in the current state as a variable - setState(prevState => {change})
+
+  const [state, setState] = useState(0);
+
+  function decrementCount() {
+    setState((prevCount) => {
+      return prevCount - 1;
+    });
+  }
+  function incrementCount() {
+    setState((prevCount) => {
+      return prevCount + 1;
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{state}</span>
+      <button onClick={incrementCount}>+</button>
+    </>
   );
 }
 
